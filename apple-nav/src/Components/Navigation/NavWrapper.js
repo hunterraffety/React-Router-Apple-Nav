5acf606a1b8cf4f0c7b8742aec17b5ee20f65e5d
hsrsbd <hsraffety@gmail.com>
2019-06-11 15:00:28 -0700

    updates

--- apple-nav/src/Components/Navigation/NavWrapper.js
+++ apple-nav/src/Components/Navigation/NavWrapper.js
@@ -1,9 +1,3 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-
-function App() {
-  return <div className='App' />;
-}
-
-export default App;
