b3a462709dc8515eca87d8a69cf4d9264fc3172b
hsrsbd <hsraffety@gmail.com>
2019-06-11 15:44:14 -0700

    updates.

--- apple-nav/src/Components/Navigation/NavWrapper.js
+++ apple-nav/src/Components/Navigation/NavWrapper.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavHeader = styled.div`
+  width: 100%;
+  background: #313131;
+  height: 44px;
+`;
 
 const NavWrapper = () => {
-  return <h1>lasdlkasdklasd</h1>;
+  return <NavHeader />;
 };
 
 export default NavWrapper;
